386acd0e0e8e91b25c9e981a406702c6ce1dc5eb
wbhpig <wbh123456kevin@126.com>
2024-05-29 15:44:19 +0800

    Changes

---
+++ app/src/main/java/com/team4/cardcase/foreground/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.team4.cardcase.foreground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team4.cardcase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
